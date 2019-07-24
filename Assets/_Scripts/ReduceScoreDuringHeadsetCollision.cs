51e0bfce92f23b48d07d2f08398200d73fe5ec5e
Daniel Isenmann <daniel.isenmann@gmail.com>
2019-07-24 20:58:23 +0200

    Add script for decreasing score if player hits a wall and also stays in the wall. Combo factor will be reset

---
+++ Assets/_Scripts/ReduceScoreDuringHeadsetCollision.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ReduceScoreDuringHeadsetCollision : MonoBehaviour
+{
+    private VRTK.VRTK_HeadsetCollision CollisionDetection;
+    private ScoreHandling ScoreHandling;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CollisionDetection = GetComponent<VRTK.VRTK_HeadsetCollision>();
+        CollisionDetection.HeadsetCollisionDetect += CollisionDetection_HeadsetCollisionDetect;
+        ScoreHandling = GameObject.FindGameObjectWithTag("ScoreHandling").GetComponent<ScoreHandling>();
+    }
+
+    private void OnDestroy()
+    {
+        CollisionDetection.HeadsetCollisionDetect -= CollisionDetection_HeadsetCollisionDetect;
+    }
+
+    private void CollisionDetection_HeadsetCollisionDetect(object sender, VRTK.HeadsetCollisionEventArgs e)
+    {
+        ScoreHandling.ResetComboFactor();
+        ScoreHandling.DecreaseScore(1000);
+    }
+}
