723cfdb5243438e184af2262ba8f53e6d3a3dad7
Daniel Isenmann <daniel.isenmann@gmail.com>
2019-09-07 21:36:06 +0200

    Split hghscore text line up into two seperated text fields for better alignement. Also highlight the username of the current user and add script for the highscore in the menu

--- Assets/_Scripts/FillHighscoreInMenu.cs
+++ Assets/_Scripts/FillHighscoreInMenu.cs
@@ -2,37 +2,50 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class FillHighscore : MonoBehaviour
+public class FillHighscoreInMenu : MonoBehaviour
 {
     public Text[] Highscore;
     public Text[] HighscoreName;
-    public Text CurrentScore;
     public Text HighScoreTitle;
 
     private HighScore.HighScore highScore = new HighScore.HighScore();
     private HighScore.HighScoreLocal highScoreLocal = new HighScore.HighScoreLocal();
 
     private SongSettings SongSettings;
-    private ScoreHandling ScoreHandling;
 
-    private void Awake()
+    public void Awake()
     {
         SongSettings = GameObject.FindGameObjectWithTag("SongSettings").GetComponent<SongSettings>();
-        ScoreHandling = GameObject.FindGameObjectWithTag("ScoreHandling").GetComponent<ScoreHandling>();
+    }
+
+    public void ShowHighscore()
+    {
+        foreach (var entry in Highscore)
+        {
+            entry.text = String.Empty;
+        }
+
+        foreach (var entry in HighscoreName)
+        {
+            entry.text = String.Empty;
+        }
+
+        if (SongSettings == null)
+        {
+            SongSettings = GameObject.FindGameObjectWithTag("SongSettings").GetComponent<SongSettings>();
+        }
+
         var playingMethod = SongSettings.CurrentSong.PlayingMethods[SongSettings.CurrentSong.SelectedPlayingMethod]?.CharacteristicName;
         if (playingMethod == null || playingMethod.Equals("Standard", StringComparison.InvariantCultureIgnoreCase))
         {
             playingMethod = string.Empty;
         }
 
-        highScoreLocal.AddHighScoreToSong(SongSettings.CurrentSong.Hash, PlayerPrefs.GetString("Username"), SongSettings.CurrentSong.SelectedDifficulty, playingMethod, ScoreHandling.ActualScore);
-
         if (PlayerPrefs.GetInt("UseGlobalHighscore") == 1)
         {
             HighScoreTitle.text = "GLOBAL HIGHSCORES";
 
-            highScore.AddHighScoreToSong(SongSettings.CurrentSong.Hash, PlayerPrefs.GetString("Username"), SongSettings.CurrentSong.Name, SongSettings.CurrentSong.SelectedDifficulty, playingMethod, ScoreHandling.ActualScore);
-            var highscoreList = highScore.GetFirstTenHighScoreOfSong(SongSettings.CurrentSong.Hash, SongSettings.CurrentSong.SelectedDifficulty, playingMethod);
+            var highscoreList = highScore.GetFirstTenHighScoreOfSong(SongSettings.CurrentSong.Hash, SongSettings.CurrentSong.SelectedDifficulty, playingMethod, false);
 
             for (int i = 0; i < highscoreList.Count; i++)
             {
@@ -55,7 +68,6 @@ public class FillHighscore : MonoBehaviour
             HighScoreTitle.text = "LOCAL HIGHSCORES";
 
             var highscoreListLocal = highScoreLocal.GetFirstTenHighScoreOfSong(SongSettings.CurrentSong.Hash, SongSettings.CurrentSong.SelectedDifficulty, playingMethod);
-            string formatStringLocal = Highscore[0].text;
 
             for (int i = 0; i < highscoreListLocal.Count; i++)
             {
@@ -63,7 +75,5 @@ public class FillHighscore : MonoBehaviour
                 HighscoreName[i].text = highscoreListLocal[i].Username;
             }
         }
-
-        CurrentScore.text = ScoreHandling.ActualScore.ToString();
     }
 }
