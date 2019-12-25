2d717f1806d403d95b5e48df5ae54547737e50f2
dhcdht <dhcdht@qq.com>
2019-12-25 23:41:26 +0800

    减弱特效；允许 bad cut ；反馈手感

--- Assets/_Scripts/Saber.cs
+++ Assets/_Scripts/Saber.cs
@@ -4,13 +4,14 @@ using DG.Tweening;
 
 public class Saber : MonoBehaviour
 {
-    public LayerMask layer;
+    public LayerMask rightLayer;
+    public LayerMask wrongLayer;
     private Vector3 previousPos;
     private Slice slicer;
 
     private float impactMagnifier = 120f;
     private float collisionForce = 0f;
-    private float maxCollisionForce = 4000f;
+    private float maxCollisionForce = 400f;
     private VRTK_ControllerReference controllerReference;
 
     private ScoreHandling scoreHandling;
@@ -33,13 +34,11 @@ public class Saber : MonoBehaviour
 
     private float Pulse()
     {
-        var hapticStrength = 0f;
+        var hapticStrength = GetHapticStrength();
 
-        if (VRTK_ControllerReference.IsValid(controllerReference))
+        if (hapticStrength >= 0.0f)
         {
-            collisionForce = VRTK_DeviceFinder.GetControllerVelocity(controllerReference).magnitude * impactMagnifier;
-            hapticStrength = collisionForce / maxCollisionForce;
-            VRTK_ControllerHaptics.TriggerHapticPulse(controllerReference, hapticStrength, 0.5f, 0.01f);
+            VRTK_ControllerHaptics.TriggerHapticPulse(controllerReference, hapticStrength, 0.2f, 0.01f);
         }
         else
         {
@@ -56,17 +55,11 @@ public class Saber : MonoBehaviour
     void Update()
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 1f, layer))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 1f, rightLayer))
         {
-            float hapticStrength = 0f;
-
-            if (VRTK_ControllerReference.IsValid(controllerReference))
-            {
-                collisionForce = VRTK_DeviceFinder.GetControllerVelocity(controllerReference).magnitude * impactMagnifier;
-                hapticStrength = collisionForce / maxCollisionForce;
-            }
-
-            if (hapticStrength > 0.05f)
+            bool isGoodCut = false;
+            float hapticStrength = GetHapticStrength();
+            if (hapticStrength > 0.1f)
             {
                 if (!string.IsNullOrWhiteSpace(hit.transform.tag) && hit.transform.CompareTag("CubeNonDirection"))
                 {
@@ -75,23 +68,42 @@ public class Saber : MonoBehaviour
                         Vector3.Angle(transform.position - previousPos, -hit.transform.up) > 130 ||
                         Vector3.Angle(transform.position - previousPos, -hit.transform.right) > 130)
                     {
-                        SliceObject(hit.transform);
+                        isGoodCut = true;
                     }
                 }
                 else
                 {
                     if (Vector3.Angle(transform.position - previousPos, hit.transform.up) > 130)
                     {
-                        SliceObject(hit.transform);
+                        isGoodCut = true;
                     }
                 }
             }
+
+            SliceObject(hit.transform, isGoodCut);
+        }
+        else if (Physics.Raycast(transform.position, transform.forward, out hit, 1f, wrongLayer))
+        {
+            SliceObject(hit.transform, false);
         }
         
         previousPos = transform.position;
     }
 
-    private void SliceObject(Transform hittedObject)
+    private float GetHapticStrength()
+    {
+        var hapticStrength = 0f;
+
+        if (VRTK_ControllerReference.IsValid(controllerReference))
+        {
+            collisionForce = VRTK_DeviceFinder.GetControllerVelocity(controllerReference).magnitude * impactMagnifier;
+            hapticStrength = collisionForce / maxCollisionForce;
+        }
+
+        return hapticStrength;
+    }
+
+    private void SliceObject(Transform hittedObject, bool isGoodCut)
     {
         var cutted = slicer.SliceObject(hittedObject.gameObject);
         var go = Instantiate(hittedObject.gameObject);
@@ -118,24 +130,19 @@ public class Saber : MonoBehaviour
             cut.transform.DOScale(0, 1f);
         }
 
-        AudioSource audioSource = null;
-
-        if (audioHandling.UseSoundFX)
-        {
-            audioSource = go.AddComponent<AudioSource>();
-            audioSource.volume = 0.15f;
-            audioSource.clip = audioHandling.GetAudioClip(cutDirection);
-            audioSource.loop = false;
-            audioSource.pitch = PitchValue(lineLayer);
-        }
-
         go.transform.SetPositionAndRotation(hittedObject.position, hittedObject.rotation);
 
         var strength = Pulse();
-        AddPointsToScore(strength);
+        if (isGoodCut)
+        {
+            DoGoodCut(strength, go, cutDirection, lineLayer);
+        } 
+        else
+        {
+            DoBadCut();
+        }
 
         Destroy(hittedObject.gameObject);
-        audioSource?.Play();
         Destroy(go, 2f);
     }
 
@@ -155,9 +162,26 @@ public class Saber : MonoBehaviour
         return pitch;
     }
 
-    private void AddPointsToScore(float strength)
+    private void DoGoodCut(float strength, GameObject go, NotesSpawner.CutDirection cutDirection, int lineLayer)
     {
+        AudioSource audioSource = null;
+        if (audioHandling.UseSoundFX)
+        {
+            audioSource = go.AddComponent<AudioSource>();
+            audioSource.volume = 0.15f;
+            audioSource.clip = audioHandling.GetAudioClip(cutDirection);
+            audioSource.loop = false;
+            audioSource.pitch = PitchValue(lineLayer);
+        }
+        audioSource?.Play();
+
         scoreHandling.IncreaseScore(System.Convert.ToInt32(10 + (strength * 100)));
         scoreHandling.IncreaseComboHits();
+    }
+
+    private void DoBadCut()
+    {
+        scoreHandling.DecreaseScore(50);
+        scoreHandling.ResetComboFactor();
     }
 }
